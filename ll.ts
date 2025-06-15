5b68a5865ff8b3a7849b4db03663abd45c9753ca
Innovative <martyniak.innovative@gmail.com>
2025-06-15 08:39:15 +0200

    feat: sync shell and stderr out

--- ll.ts
+++ ll.ts
@@ -1,7 +1,28 @@
+export function lls(command: string[]) {
+  let stdout = "";
+  let stderr = "";
+
+  const promise = new Promise((res, rej) => {
+    ll(command, (data, err) => {
+      stdout += data;
+      stderr += err;
+    }, (err) => {
+      if (err) {
+        return rej(err);
+      }
+
+      res({ stdout, stderr });
+    });
+  });
+
+  return promise;
+}
+
 export function ll(
   command: string[],
   change?: (data: string) => void,
   finished?: (err?: Error) => void,
+  stderr?: (error: string) => void,
 ) {
   const cmd = new Deno.Command(command[0], {
     args: command.slice(1),
@@ -15,7 +36,8 @@ export function ll(
     process.stdout.pipeTo(
       new WritableStream({
         write(chunk) {
-          change && change(new TextDecoder().decode(chunk));
+          const text = new TextDecoder().decode(chunk);
+          change && change(text);
         },
         close() {
           finished && finished();
@@ -29,6 +51,8 @@ export function ll(
     process.stderr.pipeTo(
       new WritableStream({
         write(chunk) {
+          const text = new TextDecoder().decode(chunk);
+          stderr && stderr(text);
         },
         close() {
         },
